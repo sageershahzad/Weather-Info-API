2a4cbac59eea2ff158d17761e1b6afeefd5d18d2
rudph1 <rudph1@ac.uk>
2020-02-14 07:43:39 +0000

    Updates

---
+++ precache-manifest.eb9424d9a57b3b3671109198b76ccef9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather-Info-API/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "8f5ae325f5f58a55a559",
+    "url": "/Weather-Info-API/static/css/main.2bb85c33.chunk.css"
+  },
+  {
+    "revision": "5d9d5d7d851021bbed51",
+    "url": "/Weather-Info-API/static/js/runtime~main.eb083625.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weather-Info-API/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "4708ac002fc904a9e985",
+    "url": "/Weather-Info-API/static/js/2.f3b9e38e.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather-Info-API/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "8f5ae325f5f58a55a559",
+    "url": "/Weather-Info-API/static/js/main.002a489c.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather-Info-API/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather-Info-API/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather-Info-API/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "4708ac002fc904a9e985",
+    "url": "/Weather-Info-API/static/css/2.3cbc164c.chunk.css"
+  },
+  {
+    "revision": "5666f9973fc86afa340cf5e93669ea3d",
+    "url": "/Weather-Info-API/index.html"
+  }
+];
